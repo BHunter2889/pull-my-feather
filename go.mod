e25896d99f221024a7a1ce6923a781bb3b976753
Hunter <bhunter8989@icloud.com>
2019-08-27 14:12:26 -0500

    init lambda impl

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module pull-my-feather
 
 go 1.12
+
+require (
+	github.com/BHunter2889/da-fish-alexa v0.0.0-20181128203011-3dc93162c389
+	github.com/aws/aws-lambda-go v1.13.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	crand "crypto/rand"
 	"encoding/binary"
 	"encoding/json"
+	"github.com/BHunter2889/da-fish-alexa/alexa"
+	"github.com/aws/aws-lambda-go/lambda"
 	"log"
 	"math/rand"
 
@@ -24,13 +27,55 @@ type pillow struct {
 	Feathers []Feather `json:"feathers"`
 }
 
-func main() {
+func IntentDispatcher(ctx context.Context, request alexa.Request) alexa.Response {
+	log.Print("Intent Dispatcher")
+	var response alexa.Response
+	switch request.Body.Intent.Name {
+	case "PullFeatherIntent":
+		log.Print("INTENT_DISPATCH: TodaysFishRatingIntent")
+		response = HandlePullFeatherIntent(ctx, request)
+	case alexa.HelpIntent:
+		log.Print("INTENT_DISPATCH: HelpIntent")
+		response = HandleHelpIntent(ctx, request)
+	case "AboutIntent":
+		log.Print("INTENT_DISPATCH: AboutIntent")
+		response = HandleAboutIntent(ctx, request)
+	default:
+		log.Print("INTENT_DISPATCH: Default Response")
+		response = HandlePullFeatherIntent(ctx, request)
+	}
+	log.Print("RESPONSE: ", response)
+	return response
+}
+
+// TODO
+func HandleAboutIntent(ctx context.Context, request alexa.Request) alexa.Response {
+	return alexa.Response{}
+}
+
+// TODO
+func HandleHelpIntent(ctx context.Context, request alexa.Request) alexa.Response {
+	return alexa.Response{}
+}
+
+func HandlePullFeatherIntent(ctx context.Context, request alexa.Request) alexa.Response {
 	pillow := pillow{}
 	if err := pluckBirdStuffPillow(feathers, &pillow); err != nil {
 		log.Fatalf("Failed to parse json string with error: %s", err)
 	}
 	metaFeather := pillow.Feathers[rand.Intn(len(pillow.Feathers))]
 	log.Print(metaFeather)
+	// TODO: Populate response
+	return alexa.Response{}
+}
+
+func Handler(ctx context.Context, request alexa.Request) (response alexa.Response, error error) {
+	log.Print("Begin Handler")
+	return IntentDispatcher(ctx, request), nil
+}
+func main() {
+	log.Print("Begin Main")
+	lambda.Start(Handler)
 }
 
 func pluckBirdStuffPillow(jsonString string, out *pillow) error {
