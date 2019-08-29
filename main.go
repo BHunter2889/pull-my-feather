58fafd3ee8124b6e50475b43b4a4599ac6f897f7
Brandon Hunter <bhunter8989@icloud.com>
2019-08-29 14:25:34 -0500

    add ssml anticipation build

--- main.go
+++ main.go
@@ -72,7 +72,7 @@ func HandlePullFeatherIntent(ctx context.Context, request alexa.Request) alexa.R
 	log.Print(date)
 
 	ssmlBuilder := alexa.SSMLBuilder{}
-	ssmlBuilder.Say("And the winner is... ") // TODO: Excited
+	ssmlBuilder.Say("And <prosody pitch='medium'> the winner </prosody> <prosody pitch='high' rate='x-slow'> is... </prosody> ") // TODO: Excited
 	ssmlBuilder.Pause("300")
 	ssmlBuilder.Say(metaFeather.Recipient) // TODO: Exclaim!
 	ssmlBuilder.Pause("500")
